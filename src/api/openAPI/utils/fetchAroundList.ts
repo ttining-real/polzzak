fb54801692787d7c6a251926d308da0d4f087473
ttining <ttining.real@gmail.com>
2025-05-20 16:12:07 +0900

    [Modify] fetchAroundList 수정
    - contentTypeId 추가
    - mapDataType 파일 추가 (타입 정의 분리)

--- src/api/openAPI/utils/fetchAroundList.ts
+++ src/api/openAPI/utils/fetchAroundList.ts
@@ -5,12 +5,14 @@ import { client } from '@/api/openAPI/client';
 interface FetchAroundListParams {
   mapX: number;
   mapY: number;
+  contentTypeId?: string;
   radius?: number;
 }
 
 export async function fetchAroundList({
   mapX,
   mapY,
+  contentTypeId,
   radius = 3000,
 }: FetchAroundListParams): Promise<AxiosResponse<any>> {
   const response = await client.get(`/locationBasedList1`, {
@@ -18,6 +20,7 @@ export async function fetchAroundList({
       mapX,
       mapY,
       radius,
+      ...(contentTypeId ? { contentTypeId } : {}),
     },
   });
 
---
+++ src/types/mapDataType.ts
@@ -0,0 +1,23 @@
+export interface aroundDataTypes {
+  addr1: string;
+  addr2: string;
+  areacode: string;
+  booktour: string;
+  cat1: string;
+  cat2: string;
+  cat3: string;
+  contentid: string;
+  contenttypeid: string;
+  createdtime: string;
+  dist: string;
+  firstimage: string;
+  firstimage2: string;
+  cpyrhtDivCd: string;
+  mapx: string;
+  mapy: string;
+  mlevel: string;
+  modifiedtime: string;
+  sigungucode: string;
+  tel: string;
+  title: string;
+}
