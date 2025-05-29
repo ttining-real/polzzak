27547c6ef1ea31c628beb775961d16db8085e775
ttining <ttining.real@gmail.com>
2025-05-29 16:33:46 +0900

    [Feature] 거리 계산 함수 생성
    - getDistance 유틸 함수 생성

---
+++ src/lib/getDistance.ts
@@ -0,0 +1,20 @@
+// 거리 계산 함수 (Haversine 공식)
+export function getDistance(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number,
+) {
+  const R = 6371e3; // 지구 반지름(m)
+  const φ1 = (lat1 * Math.PI) / 180;
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; // in meters
+}
