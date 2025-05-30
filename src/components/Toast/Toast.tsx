335abf918ca933b30b13f6602e5162f1d5d09a3c
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-30 17:26:17 +0900

    [Modify] z-index 수정
    # 245

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -7,7 +7,7 @@ function Toast() {
 
   return (
     <div
-      className={`fixed left-1/2 z-10 flex -translate-x-[50%] items-center gap-4 rounded-md bg-black/75 px-4 py-2 ${position}`}
+      className={`fixed left-1/2 z-10 flex -translate-x-[50%] items-center gap-4 rounded-md bg-black/75 px-4 py-2 ${position} z-[999]`}
     >
       <p className="font-regular fs-14 whitespace-nowrap text-white">
         {message}
