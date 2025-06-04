c51c25686d1e6a27cf774177819ff15d679bd36b
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-04 18:51:54 +0900

    [Modify] 스토어 대신 url에 추가된 contentTypeId로 필요 값 사용
    # 263

--- src/pages/Contents/ViewDetails.tsx
+++ src/pages/Contents/ViewDetails.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom';
 
 import { useGetDetailCommon } from '@/api/openAPI';
+import { fetchContentDetail } from '@/api/openAPI/utils/fetchContentDetail';
 import supabase from '@/api/supabase';
 import Button from '@/components/Button/Button';
 import Details from '@/components/Contents/Details';
@@ -11,25 +17,30 @@ import Icon from '@/components/Icon/Icon';
 import { FavoirteType, PolzzakType } from '@/components/Input/SelectMenu';
 import Loader from '@/components/Loader/Loader';
 import UserMenu, { MenuItemTypes } from '@/components/UserMenu/UserMenu';
+import { filterNameToType } from '@/lib/filterMap';
 import { useAuthStore } from '@/store/useAuthStore';
 import { useDialogStore } from '@/store/useDialogStore';
 import { useHeaderStore } from '@/store/useHeaderStore';
-import { useSearchStore } from '@/store/useSearchStore';
+// import { useSearchStore } from '@/store/useSearchStore';
 
 function ViewDetails() {
   const { id } = useParams();
+  const [searchParams] = useSearchParams();
+  const contentTypeName = searchParams.get('category');
+  const contentTypeId = filterNameToType(contentTypeName ?? '');
   const navigate = useNavigate();
   const location = useLocation();
   const [isLoading, setIsLoading] = useState('');
   const [radioList, setRadioList] = useState<
     FavoirteType[] | PolzzakType[] | null
   >(null);
+  const [info, setInfo] = useState<Record<string, string | undefined>>({});
   const [isMyContent, setIsMyContent] = useState(false);
   const [selectedFolderId, setSelectedFolderId] = useState<string | null>(null);
-  const detailData = useSearchStore((state) => state.detailData);
+  // const detailData = useSearchStore((state) => state.detailData);
   const setContentsTitle = useHeaderStore((state) => state.setContentsTitle);
   const { isOpenId, openModal, closeModal } = useDialogStore();
-  const info = detailData?.filter((item) => item.contentid.toString() === id);
+  // const info = detailData?.filter((item) => item.contentid.toString() === id);
   const rawData = useGetDetailCommon(id as string);
   const data = rawData ?? null;
   const { user } = useAuthStore();
@@ -64,6 +75,10 @@ function ViewDetails() {
   ];
 
   useEffect(() => {
+    if (!id || !contentTypeId) navigate('/', { replace: true });
+  }, [id, contentTypeId, navigate]);
+
+  useEffect(() => {
     if (data?.title) {
       setContentsTitle(data.title);
     }
@@ -71,6 +86,17 @@ function ViewDetails() {
       setContentsTitle(null);
     };
   }, [data, setContentsTitle]);
+
+  useEffect(() => {
+    if (!id || !contentTypeId) return;
+
+    const getContentDetail = async () => {
+      const data = await fetchContentDetail(id, contentTypeId, true);
+      console.log(data);
+      setInfo(data);
+    };
+    getContentDetail();
+  }, [id, contentTypeId]);
 
   useEffect(() => {
     if (!id || !userId) return;
@@ -205,15 +231,15 @@ function ViewDetails() {
   return (
     <div className="flex flex-col gap-4">
       <figure className="bg-primary/10 flex h-full min-h-[230px] w-full flex-col items-center justify-center rounded-2xl">
-        {info.length > 0 ? (
-          info[0].image ? (
+        {info ? (
+          info.image ? (
             <>
               <img
-                src={info[0].image}
+                src={info.image}
                 alt=""
                 className="h-full w-full rounded-2xl"
               />
-              <figcaption className="sr-only">{info[0].title}</figcaption>
+              <figcaption className="sr-only">{info.title}</figcaption>
             </>
           ) : (
             <>
@@ -230,8 +256,8 @@ function ViewDetails() {
         )}
       </figure>
       <UserMenu menus={userMenu} />
-      {data && info.length > 0 ? (
-        <Details info={info[0]} data={data} />
+      {data && info ? (
+        <Details info={info} data={data} />
       ) : (
         <div>데이터를 불러오는 중 입니다.</div>
       )}
@@ -245,8 +271,8 @@ function ViewDetails() {
             userId={userId}
             info={{
               contenttypeid: data?.contenttypeid,
-              title: info[0].title,
-              addr1: info[0].addr1,
+              title: info?.title ?? '타이틀이 없어요.',
+              addr1: info.addr1,
             }}
             setIsMyContent={setIsMyContent}
           />
