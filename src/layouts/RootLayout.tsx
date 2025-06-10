27b7528aa1963e376d3cf1b963126d038b84274f
ttining <ttining.real@gmail.com>
2025-06-10 17:58:27 +0900

    [Modify] RootLayout 수정
    - HEADER_TITLES 수정 (이메일 찾기 제거)

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -12,7 +12,6 @@ import { useHeaderStore } from '@/store/useHeaderStore';
 const HEADER_TITLES: Record<string, string> = {
   '/': '🐰폴짝🐰',
   '/login': '로그인',
-  '/login/find-email': '이메일 찾기',
   '/login/reset-password': '비밀번호 재설정',
   '/search': '검색',
   '/map': '지도',
