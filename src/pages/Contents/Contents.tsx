fd124ee016dd0f0f05edacf2b70e719342fea4c5
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-06-12 16:46:57 +0900

    [Modify] cite 태그 위치 변경
    - ViewDetail 파일 안으로 변경
    
    # 283

--- src/pages/Contents/Contents.tsx
+++ src/pages/Contents/Contents.tsx
@@ -4,10 +4,6 @@ function Contents() {
   return (
     <main className="no-scrollbar flex flex-1 flex-col gap-4 overflow-auto">
       <Outlet />
-
-      <cite className="text-gray06 bg-gray01 fs-13 ls lh p-6 text-center">
-        ※ 한국관광공사 TourAPI 4.0을 통해 제공받은 데이터 입니다.
-      </cite>
     </main>
   );
 }
