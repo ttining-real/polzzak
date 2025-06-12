04fd27e006e6343eff3a4d6ad7ff71ab995e1065
ttining <ttining.real@gmail.com>
2025-06-12 14:17:12 +0900

    [Remove] 불필요한 파일 제거
    - REGISTER_STEP.ts
    - Register 파일 내에서 처리

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -5,7 +5,21 @@ import { Progress } from '@/components/Register/progress';
 import { Step1, Step2, Step3 } from '@/pages/Register';
 
 import NotFound from '../NotFound';
-import { REGISTER_STEP } from './REGISTER_STEP';
+
+const REGISTER_STEP = [
+  {
+    step: 1,
+    title: '로그인에 사용할 이메일 주소를 입력해 주세요.',
+  },
+  {
+    step: 2,
+    title: '로그인에 사용할 비밀번호를 입력해 주세요.',
+  },
+  {
+    step: 3,
+    title: '폴짝에서 사용할 닉네임을 입력해 주세요.',
+  },
+];
 
 function Register() {
   const [searchParams, setSearchParams] = useSearchParams();
